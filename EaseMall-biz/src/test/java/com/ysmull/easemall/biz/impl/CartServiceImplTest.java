ef105c6c77bbf9b556ccdd3128223fb2be63d753
ysmull <ysmull@aliyun.com>
2018-03-20 15:41:21 +0800

    t

---
+++ EaseMall-biz/src/test/java/com/ysmull/easemall/biz/impl/CartServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.ysmull.easemall.biz.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CartServiceImplTest {
+
+    @Test
+    public void getCart() {
+    }
+
+    @Test
+    public void addCart() {
+    }
+}
